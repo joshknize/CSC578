aa3bffba817e088166646c1ec405de7873d67429
Josh Knize <jknize@aiscalar.cdm.depaul.edu>
2024-11-09 18:30:51 -0600

    11/9 evaluate default backbone checkpoint

---
+++ python/write_filtered_annotations.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+os.chdir('/home/jknize/main/repo/CSC578/detectron2')
+
+chosen_class_ids = [1, 18, 44, 62, 84]  # (person, dog, bottle, chair, book)
+
+annotations_path = '../datasets/coco/annotations/instances_train2017.json'
+
+# Load the annotations
+with open(annotations_path, 'r') as f:
+    coco_annotations = json.load(f)
+
+# Filter annotations for the chosen classes
+filtered_annotations = []
+for annotation in coco_annotations['annotations']:
+    if annotation['category_id'] in chosen_class_ids:
+        filtered_annotations.append(annotation)
+
+# Filter the categories as well
+filtered_categories = [category for category in coco_annotations['categories'] if category['id'] in chosen_class_ids]
+
+# Update the original coco json with the filtered annotations
+coco_annotations['annotations'] = filtered_annotations
+coco_annotations['categories'] = filtered_categories
+
+# Save the filtered annotations to a new file
+filtered_annotations_path = '../datasets/coco/annotations/filtered_instances_train2017.json'
+with open(filtered_annotations_path, 'w') as f:
+    json.dump(coco_annotations, f)
